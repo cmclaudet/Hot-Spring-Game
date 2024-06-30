0d2c3e434f79a243cfba806d4d395ef5260a76e8
cmclaudet <cmclaudet@gmail.com>
2024-06-30 17:35:16 +0200

    Add all beds to guest ui panels for existing guests

--- Scripts/guest_in_room_panel.gd
+++ Scripts/guest_in_room_panel.gd
@@ -1,10 +1,12 @@
 class_name GuestInRoomPanel extends VBoxContainer
 
+var room_panel_bed_scene = preload("res://Scenes/Matching/room_panel_bed.tscn")
+
 @export var guest_name : Label
 @export var guest_picture : TextureRect
 @export var days_remaining_label : Label
 @export var room_name : Label
-@export var room_picture : TextureRect
+@export var bed_container : HBoxContainer
 
 func init(guest_state : GuestState):
 	set_guest(guest_state.guest)
@@ -17,7 +19,13 @@ func set_guest(guest : Guest):
 
 func set_room(room : Room):
 	room_name.text = room.get_room_name()
-	room_picture.texture = room.beds[0].icon
+	for child in bed_container.get_children():
+		child.queue_free()
+
+	for bed : Bed in room.beds:
+		var bed_instance = room_panel_bed_scene.instantiate()
+		bed_container.add_child(bed_instance)
+		bed_instance.init(bed)
 
 func set_days_remaining(guest_state : GuestState):
 	var days_remaining = guest_state.guest.stay_duration - guest_state.days_stayed
